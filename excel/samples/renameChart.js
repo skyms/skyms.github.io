e3be86466bd4dcdf529af2e624a27262b61aefce
skyms <ylu@microsoft.com>
2015-06-18 15:38:11 -0700

    Update renameChart.js

--- excel/samples/renameChart.js
+++ excel/samples/renameChart.js
@@ -1,3 +1,3 @@
 var ctx = new Excel.ExcelClientContext();
-ctx.workbook.worksheets.getItem("Sheet1").charts.getItemAt(0).name = "Chart 1";
+ctx.workbook.worksheets.getItem("Sheet1").charts.getItemAt(0).name = "Chart1";
 ctx.executeAsync().then();
