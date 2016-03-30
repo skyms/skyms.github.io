e6310d25e5be21d20962b23c362257566fea7374
skyms <ylu@microsoft.com>
2016-03-30 12:51:00 -0700

    update filter

--- office-js-snippet-explorer-master/excel-snippets/filter.js
+++ office-js-snippet-explorer-master/excel-snippets/filter.js
@@ -2,10 +2,10 @@
 Excel.run(function (ctx) {
 	var range = ctx.workbook.worksheets.getItem("Sheet1").getRange("A1:C3");
 	range.values = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-	ctx.workbook.tables.add(range, false);
+	ctx.workbook.tables.add("Sheet1!A1:C3", false);
 
 	var column = ctx.workbook.tables.getItem("Table1").columns.getItemAt(0);
-    column.filter.applyDynamicFilter(Excel.DynamicFilterCriteria.aboveAverage);
+	column.filter.applyDynamicFilter(Excel.DynamicFilterCriteria.aboveAverage);
 
 	return ctx.sync();
 }).catch(function (error) {
