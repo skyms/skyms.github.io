239fa132fa287b6589f2af1f2b49496d5d2b31a8
skyms <ylu@microsoft.com>
2016-08-08 21:11:42 -0700

    fixing bugs

--- office-js-snippet-explorer-master/excel-snippets/addChart.js
+++ office-js-snippet-explorer-master/excel-snippets/addChart.js
@@ -1,6 +1,6 @@
 
 Excel.run(function (ctx) {
-	var range = ctx.workbook.getActiveWorksheet().getUsedRange();
+	var range = ctx.workbook.worksheets.getActiveWorksheet().getUsedRange();
 	ctx.workbook.worksheets.getActiveWorksheet().charts.add("ColumnClustered", range , Excel.ChartSeriesBy.auto);
 	return ctx.sync();
 }).catch(function (error) {
