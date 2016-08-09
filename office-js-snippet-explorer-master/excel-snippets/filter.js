f6ccc4bb9b485addea1fc6c65141a98fcce6da66
skyms <ylu@microsoft.com>
2016-08-08 21:01:07 -0700

    fixing bugs

--- office-js-snippet-explorer-master/excel-snippets/filter.js
+++ office-js-snippet-explorer-master/excel-snippets/filter.js
@@ -1,7 +1,7 @@
 
 Excel.run(function (ctx) {
 	var filterColumn = ctx.workbook.tables.getItemAt(0).columns.getItemAt(2);
-	column.filter.applyDynamicFilter(Excel.DynamicFilterCriteria.aboveAverage);
+	filterColumn.filter.applyDynamicFilter(Excel.DynamicFilterCriteria.aboveAverage);
 
 	return ctx.sync();
 }).catch(function (error) {
